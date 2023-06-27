a37d1989e646f8c8933fb478bc775969d5dc8db1
joao-trajano <joao.trajanosouza@gmail.com>
2023-06-27 13:45:37 -0300

    implementado o middleware de autenticação

--- src/app/routes/TaskRouter.js
+++ src/app/routes/TaskRouter.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import Task from '../controllers/TaskController.js';
+import { isAuthenticated } from '../../middlewares/isAuthenticated.js';
 
 const routerTask = Router();
 
 routerTask
-  .get('/tasks', Task.index)
-  .get('/tasks/:id', Task.show)
-  .post('/tasks/create', Task.store)
-  .put('/tasks/:id', Task.update)
-  .delete('/tasks/:id', Task.delete);
+  .get('/tasks', isAuthenticated, Task.index)
+  .get('/tasks/:id', isAuthenticated, Task.show)
+  .post('/tasks/create', isAuthenticated, Task.store)
+  .put('/tasks/:id', isAuthenticated, Task.update)
+  .delete('/tasks/:id', isAuthenticated, Task.delete);
 
 export default routerTask;
--- src/app/routes/UserRouter.js
+++ src/app/routes/UserRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { isAuthenticated } from '../../middlewares/isAuthenticated.js';
 import UserController from '../controllers/UserController.js';
 
 const routerUser = Router();
 
 routerUser
-  .get('/users', UserController.index)
-  .get('/user/:id', UserController.show);
+  .get('/users', isAuthenticated, UserController.index)
+  .get('/user/:id', isAuthenticated, UserController.show);
 
 export default routerUser;
